95be4360aaea8a57220f663524714feccf9ebaf7
amcaro82 <am.caro82@gmail.com>
2025-08-05 22:13:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # 👈 Habilita CORS
 
 @app.route('/calcular-cuota', methods=['POST'])
 def calcular_cuota():
