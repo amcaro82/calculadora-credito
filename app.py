97c098b26d003af4145dff4862e41b9e4306d292
amcaro82 <am.caro82@gmail.com>
2025-08-05 22:30:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,7 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route("/", methods=["GET", "HEAD"])
+def home():
+    return "API de crédito operativa", 200
